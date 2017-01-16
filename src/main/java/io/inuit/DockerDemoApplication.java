16b011d1d685bb1e0f61ca9dcc7bfdca1662f0b0
Reza Motevallizadeh <rezamt@gmail.com>
2017-01-16 21:30:11 +1100

    Bootstrap Codes

---
+++ src/main/java/io/inuit/DockerDemoApplication.java
@@ -0,0 +1,27 @@
+package io.inuit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@SpringBootApplication
+@RestController
+public class DockerDemoApplication {
+
+	@RequestMapping("/resource")
+	public Map<String,Object> home() {
+		Map<String,Object> model = new HashMap<String,Object>();
+		model.put("id", UUID.randomUUID().toString());
+		model.put("content", "Hello From Dockerland");
+		return model;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerDemoApplication.class, args);
+	}
+}
